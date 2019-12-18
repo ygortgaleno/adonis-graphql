0d1ad070761334e3d5ed7d53eb61ce3254e02f19
Ygor Galeno <ygor.tgaleno@gmail.com>
2019-12-18 12:25:40 -0200

    Initial commit

---
+++ src/healthCheck/database.js
@@ -0,0 +1,37 @@
+const { Client } = require('pg')
+
+const { DB_DATABASE, DB_HOST, DB_PASSWORD, DB_USER } = process.env
+
+const sleep = (time) => new Promise(
+  resolve => setTimeout(resolve, time)
+)
+
+const connect = async ({ retries, time }) => {
+  if (retries === 0) {
+    console.error('Cant connect into database exiting...')
+    process.exit(1)
+  }
+
+  await sleep(time)
+  console.log('Attempt to connect into database...')
+  const client = new Client({
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_DATABASE,
+    host: DB_HOST
+  })
+
+  client.connect((error) => {
+    client.end()
+
+    if (error) {
+      console.error('Failed to connect into database!')
+      console.log('Performing new attempt in 5 seconds.')
+      connect({ retries: retries - 1, time: 5000 })
+    } else {
+      console.log('Connected!')
+    }
+  })
+}
+
+connect({ retries: 5, time: 0 })
